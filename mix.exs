c09639e925a73418285c49da6bd2696d1c0895fd
Billal GHILAS <bilal@evercam.io>
2025-10-04 17:32:48 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ExFLV.MixProject do
   def project do
     [
       app: :ex_flv,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
